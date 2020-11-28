db05504d64553bd14d4c2b98251ac84822aeba46
zhenghong <zhenghong@daily.com>
2020-11-28 17:59:46 +0800

    Initial Commit

---
+++ TimeMonitor/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  TimeMonitor
+//
+//  Created by mac on 2020/11/28.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
